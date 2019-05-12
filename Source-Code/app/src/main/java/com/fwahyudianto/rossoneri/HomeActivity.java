564153adfb33269edbc76ba204b8252d01bd76a4
fwahyudianto06 <fwahyudi06@gmail.com>
2019-05-12 22:46:59 +0700

    Add Grid View

--- Source-Code/app/src/main/java/com/fwahyudianto/rossoneri/HomeActivity.java
+++ Source-Code/app/src/main/java/com/fwahyudianto/rossoneri/HomeActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.fwahyudianto.rossoneri.controllers.PlayerGridAdapter;
 import com.fwahyudianto.rossoneri.controllers.PlayerListAdapter;
 import com.fwahyudianto.rossoneri.models.Player;
 import com.fwahyudianto.rossoneri.models.PlayerData;
@@ -30,9 +33,35 @@ public class HomeActivity extends AppCompatActivity {
         oRecyclerView = findViewById(R.id.rv_main);
         oRecyclerView.setHasFixedSize(true);
 
+        /*
         setActionBarTitle("Hall of Fame");
         alData.addAll(PlayerData.List());
-        RecyclerList();
+        RecyclerList(); */
+
+        alData = new ArrayList<>();
+
+        if (savedInstanceState == null) {
+            setActionBarTitle("Hall of Fame - List View");
+            alData.addAll(PlayerData.List());
+            RecyclerList();
+            iMode = R.id.menu_list;
+        } else {
+            String strTitle = savedInstanceState.getString(m_strStateTitle);
+            ArrayList<Player> oPlayer = savedInstanceState.getParcelableArrayList(m_strStateList);
+            int iStateMode = savedInstanceState.getInt(m_strStateMode);
+
+            setActionBarTitle(strTitle);
+            alData.addAll(oPlayer);
+            setMode(iStateMode);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle oBundle) {
+        super.onSaveInstanceState(oBundle);
+        oBundle.putString(m_strStateTitle, getSupportActionBar().getTitle().toString());
+        oBundle.putParcelableArrayList(m_strStateList, alData);
+        oBundle.putInt(m_strStateMode, iMode);
     }
 
     @Override
@@ -49,6 +78,20 @@ public class HomeActivity extends AppCompatActivity {
         m_lngBackPressed = System.currentTimeMillis();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu p_oMenu) {
+        getMenuInflater().inflate(R.menu.menu_home, p_oMenu);
+
+        return super.onCreateOptionsMenu(p_oMenu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem p_oMenuItem) {
+        setMode(p_oMenuItem.getItemId());
+
+        return super.onOptionsItemSelected(p_oMenuItem);
+    }
+
     //  Method
     private void RecyclerList(){
         oRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -59,7 +102,33 @@ public class HomeActivity extends AppCompatActivity {
         oRecyclerView.setAdapter(oPlayerListAdapter);
     }
 
+    private void RecyclerGrid(){
+        oRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        PlayerGridAdapter oPlayerGridAdapter = new PlayerGridAdapter(this);
+        oPlayerGridAdapter.setPlayer(alData);
+
+        oRecyclerView.setAdapter(oPlayerGridAdapter);
+    }
+
     private void setActionBarTitle(String p_strTitle) {
         getSupportActionBar().setTitle(p_strTitle);
+    }
+
+    private void setMode(int p_iMode) {
+        String strTitle = null;
+        switch (p_iMode) {
+            case R.id.menu_list:
+                strTitle = "Hall of Fame - List View";
+                RecyclerList();
+                break;
+            case R.id.menu_grid:
+                strTitle = "Hall of Fame - Grid View";
+                RecyclerGrid();
+                break;
+        }
+
+        iMode = p_iMode;
+        setActionBarTitle(strTitle);
     }
 }
---
+++ Source-Code/app/src/main/java/com/fwahyudianto/rossoneri/controllers/PlayerGridAdapter.java
@@ -0,0 +1,84 @@
+package com.fwahyudianto.rossoneri.controllers;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.fwahyudianto.rossoneri.R;
+import com.fwahyudianto.rossoneri.models.Player;
+
+import java.util.ArrayList;
+
+/**
+ * This software, all associated documentation, and all copies are CONFIDENTIAL INFORMATION of Kalpawreska Teknologi Indonesia
+ * http://www.fwahyudianto.id
+ * ® Wahyudianto, Fajar
+ * Email 	: fwahyudi06@gmail.com
+ */
+
+public class PlayerGridAdapter extends RecyclerView.Adapter<PlayerGridAdapter.GridViewHolder> {
+    //  Variable
+    private Context oContext;
+    private ArrayList<Player> alPlayer;
+
+    //  Constructor
+    public PlayerGridAdapter(Context p_oContext) {
+        this.oContext = p_oContext;
+    }
+
+    //  Getter
+    public ArrayList<Player> getPlayer() {
+        return alPlayer;
+    }
+
+    //  Setter
+    public void setPlayer(ArrayList<Player> p_alPlayer) {
+        this.alPlayer = p_alPlayer;
+    }
+
+    @NonNull
+    @Override
+    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup oViewGroup, int iType) {
+        View oView = LayoutInflater.from(oViewGroup.getContext()).inflate(
+                R.layout.item_hall_of_fame_grid, (ViewGroup) oViewGroup, false);
+
+        return new GridViewHolder(oView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GridViewHolder oHolder, int iIndex) {
+        oHolder.tvNumber.setText(getPlayer().get(iIndex).getNumberTshirt());
+        oHolder.tvFulName.setText(getPlayer().get(iIndex).getFullName());
+
+        Glide.with(oContext)
+                .load(getPlayer().get(iIndex).getPhoto())
+                .apply(new RequestOptions().override(350, 350))
+                .into(oHolder.ivPhoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return getPlayer().size();
+    }
+
+    public class GridViewHolder extends RecyclerView.ViewHolder {
+        TextView tvNumber;
+        TextView tvFulName;
+        ImageView ivPhoto;
+
+        public GridViewHolder(@NonNull View oView) {
+            super(oView);
+
+            tvNumber = oView.findViewById(R.id.tv_number);
+            tvFulName = oView.findViewById(R.id.tv_full_name);
+            ivPhoto = oView.findViewById(R.id.img_photo);
+        }
+    }
+}
